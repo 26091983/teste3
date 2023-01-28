69c0a0f9a9260a22f2c35c50fbb6bd0454f48176
26091983 <122417082+26091983@users.noreply.github.com>
2023-01-28 19:24:29 -0300

    teste2

---
+++ teste2.py
@@ -0,0 +1,9 @@
+#este trabalho fala sobre o mundo
+""" vamos falar neste trabalho sobre coisas que acontecem no mundo de hoje!
+ E suas consequencias para o futuro da humanidade. """
+
+print("ola mundo! Como voce esta", '\n')
+print(15, 26, '\n')
+print(15 , 36 , sep=" + ", end= '\n\n')
+print(32, 56, sep=" - nathalia - ", end= '\n\n')
+print(32, 56, sep=" - nathalia - ", end= '\n\n')
